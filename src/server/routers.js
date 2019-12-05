2737d60f06c1b5d5aa30970b0838654aaa5853a2
Maria <m.olanipekun@hotmail.com>
2019-12-05 12:31:06 +0000

    basic skeleton of server files

--- src/server/routers.js
+++ src/server/routers.js
@@ -1,1 +1,29 @@
+const {
+    handleHome,
+    handlePublic,
+    postHandler,
+    getHandler
+} = require('./handlers');
+
+
+const router = (request, response) => {
+    const endpoint = request.url;
+    if (endpoint === '/') {
+        handleHome(request, response);
+    }
+    else if (endpoint.includes('public')) {
+        handlePublic(request, response);
+    }
+    else if (endpoint === '/.......') {
+        getHandler(request, response);
+    }
+    else if (endpoint === '/.......') {
+        postHandler(postHandler);
+    } else {
+        response.writeHead(404, {'Content-type': 'text/html'});
+        response.end('<h1> Page not found</h1>');
+    }
+};
+
+module.exports = router;
 // routers.js

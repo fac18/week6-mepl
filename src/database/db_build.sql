20848b5ca140b5fdb8b5eee931828260987129ba
kin-au <kinnamau@gmail.com>
2019-12-05 12:16:53 +0000

    change db build file for fruit basket

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -2,31 +2,26 @@
 
 BEGIN;
 
-DROP TABLE IF EXISTS user_input CASCADE;
-DROP TABLE IF EXISTS user_results CASCADE;
-DROP TABLE IF EXISTS choices CASCADE;
-
-CREATE TABLE "user_input" (
+CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
-  "username" varchar,
-  "current_age" int
+  "name" varchar,
+  "kitty" int DEFAULT 16
 );
 
-CREATE TABLE "user_results" (
-  "id" int,
-  "username" varchar,
-  "calculated_life_expectancy" int,
-  "years_left_to_live" int
+CREATE TABLE "shop" (
+  "id" SERIAL PRIMARY KEY,
+  "fruit" varchar,
+  "stock" int,
+  "price" int
 );
 
-CREATE TABLE "choices" (
-  "id" int PRIMARY KEY,
-  "question" varchar,
-  "yes_answer" int,
-  "no_answer" int
+CREATE TABLE "basket" (
+  "id" SERIAL PRIMARY KEY,
+  "fruit_id" int,
+  "stock" int
 );
 
-ALTER TABLE "user_results" ADD FOREIGN KEY ("id") REFERENCES "user_input" ("id");
-ALTER TABLE "user_results" ADD FOREIGN KEY ("username") REFERENCES "user_input" ("username");
+ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "basket" ("id");
+ALTER TABLE "users" ADD FOREIGN KEY ("name") REFERENCES "shop" ("id");
 
 COMMIT;
